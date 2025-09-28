c9f9e978b043aa801a371be737b647e36f69226d
anmolsah <kumarxxx242424@gmail.com>
2025-09-28 20:16:40 +0530

    feat: add endpoint to check if user is authenticated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -152,3 +152,13 @@ export const verifyEmail = async (req, res) => {
     return res.json({ success: false, message: error.message });
   }
 };
+
+
+//checking if user is authenticated or not
+export const isAuthenticated = async(req,res)=>{
+try {
+  return res.json({success:true});
+} catch (error) {
+  return res.json({ success: false, message: error.message });
+}
+}

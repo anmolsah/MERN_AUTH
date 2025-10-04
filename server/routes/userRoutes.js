132a07d91b09520ed0772e098b029f26186de786
anmolsah <kumarxxx242424@gmail.com>
2025-10-04 21:25:17 +0530

    fix: correct user data route path to '/data'

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,6 @@ import userAuth from "../middleware/userAuth.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/", userAuth, getUserData);
+userRouter.get("/data", userAuth, getUserData);
 
 export default userRouter;

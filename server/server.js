fb59ece6350dde343ac0696cf1774aad24f0bb5e
anmolsah <kumarxxx242424@gmail.com>
2025-10-04 21:13:25 +0530

    feat: add user routes and controller for fetching user data

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,6 +18,7 @@ app.get("/", (req, res) => {
   res.send("the server is working fine");
 });
 app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
 
 app.listen(port, () => {
   console.log(`Running in port : ${port}`);

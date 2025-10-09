e12c379797b6649404792113e4ff20231335fe7b
anmolsah <kumarxxx242424@gmail.com>
2025-10-09 20:19:31 +0530

    feat: enhance Login component with additional input fields and improved styling

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { assets } from "./../assets/assets";
-import { User } from "lucide-react";
+import { LockKeyhole, Mail, User } from "lucide-react";
 
 const Login = () => {
   const [state, setState] = useState("Sign Up");
@@ -11,18 +11,70 @@ const Login = () => {
         alt="logo"
         className="absolute left-5 sm;left-20 top-5 w-28 sm:w-32 cursor-pointer"
       />
-      <div>
-        <h2>{state === "Sign Up" ? "Create Account" : "Login "}</h2>
-        <p>
-          {state === "Sign Up" ? "Create Account" : "Login to your Account"}
+      <div className="bg-slate-900 p-10 rounded-lg shadow-lg width-full sm:w-96 text-indigo-300 text-sm">
+        <h2 className="text-3xl font-semibold text-white text-center mb-3">
+          {state === "Sign Up" ? "Create Account" : "Login "}
+        </h2>
+        <p className="text-center text-sm mb-6">
+          {state === "Sign Up"
+            ? "Create Your Account"
+            : "Login to your Account"}
         </p>
 
         <form>
-          <div className="mb-4 flex items-center gap-3">
-            <User />
-            <input type="text" placeholder="Full Name" required />
+          {state === "Sign Up" && (
+            <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#2e334b]">
+              <User />
+              <input
+                className="bg-transparent outline-none"
+                type="text"
+                placeholder="Full Name"
+                required
+              />
+            </div>
+          )}
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#2e334b]">
+            <Mail />
+            <input
+              className="bg-transparent outline-none"
+              type="email"
+              placeholder="example@gmail.com"
+              required
+            />
           </div>
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#2e334b]">
+            <LockKeyhole />
+            <input
+              className="bg-transparent outline-none"
+              type="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+
+          <p className="mb-4 text-indigo-500 cursor-pointer">
+            Forgot Password?
+          </p>
+          <button className="w-full bg-indigo-600 hover:bg-indigo-700 text-white py-2.5 rounded-full transition duration-300 mb-1">
+            {state}
+          </button>
         </form>
+        {state === "Sign Up" ? (
+          <p className="text-gray-400 text-center text-xs mt-4">
+            Already have an account?{" "}
+            <span onClick={()=>setState("Login")} className="text-indigo-400 cursor-pointer underline">
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p className="text-gray-400 text-center text-xs mt-4">
+            Don't have an account?{" "}
+            <span onClick={()=>setState("Sign Up")} className="text-indigo-400 cursor-pointer underline">
+              Sign Up here
+            </span>
+          </p>
+        )}
       </div>
     </div>
   );

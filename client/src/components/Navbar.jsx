144e1725c4fd8e541d454807ab04aca5d5813ded
anmolsah <kumarxxx242424@gmail.com>
2025-10-10 20:52:45 +0530

    feat: update user authentication flow to use req.userId and enhance Navbar with email verification prompt

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,22 @@ const Navbar = () => {
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
       <img src={assets.mern} alt="logo" className="w-28 sm:w-36" />
       {userData ? (
-        <div>{userData.name[0].toUpperCase()}</div>
+        <div className="w-10 h-10 flex justify-center items-center rounded-full bg-black text-white relative group">
+          {userData.name[0].toUpperCase()}
+          <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
+            <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
+              {!userData.isAccountVerified && (
+                <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer">
+                  Verify Email
+                </li>
+              )}
+
+              <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10">
+                Logout
+              </li>
+            </ul>
+          </div>
+        </div>
       ) : (
         <button
           onClick={() => navigate("/login")}
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,9 +2,9 @@ import userModel from "../models/userModel.js";
 
 export const getUserData = async (req, res) => {
   try {
-    const { userId } = req.body;
+    //const { userId } = req.body;
 
-    const user = await userModel.findById(userId);
+    const user = await userModel.findById(req.userId);
 
     if (!user) {
       return res.json({ success: false, message: "User not found" });
